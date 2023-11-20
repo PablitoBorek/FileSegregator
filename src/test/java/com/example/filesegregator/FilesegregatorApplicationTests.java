0ceaee72547548f569f2542762993cbda9b9a616
Paweł Borkowski <pawel.borkowski@hycom.pl>
2023-11-20 11:30:46 +0100

    Initial commit

---
+++ src/test/java/com/example/filesegregator/FilesegregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filesegregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesegregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
